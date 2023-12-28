efab3c6839bdcc266552b574abe0c8b2fe7688e1
berenydonmez <113702268+berenydonmez@users.noreply.github.com>
2023-12-28 22:01:57 +0300

    correct indentation

--- frontend/src/Shop/pages/profile.tsx
+++ frontend/src/Shop/pages/profile.tsx
@@ -1,6 +1,15 @@
 // Created by Georgina Alacaraz
-import { UserContextType, MyPaymentMetadata , CommunityType} from "../components/Types";
-import { onCancel, onError, onReadyForServerApproval, onReadyForServerCompletion } from "../components/Payments";
+import {
+  UserContextType,
+  MyPaymentMetadata,
+  CommunityType,
+} from "../components/Types";
+import {
+  onCancel,
+  onError,
+  onReadyForServerApproval,
+  onReadyForServerCompletion,
+} from "../components/Payments";
 import SignIn from "../components/SignIn";
 import Header from "../components/Header";
 import Typography from "@mui/material/Typography";
@@ -8,35 +17,55 @@ import { UserContext } from "../components/Auth";
 import React, { useEffect, useState } from "react";
 import MuiBottomNavigation from "../../MuiBottomNavigation";
 import ProfileCard from "../components/ProfileCard";
-import axios from 'axios';
-
+import axios from "axios";
 
 /* DEVELOPER NOTE:
 * this page facilitates the purchase of pies for pi. all of the callbacks
-* can be found on the Payments.tsx file in components file. 
+* can be found on the Payments.tsx file in components file.
 */
 
 // Make TS accept the existence of our window.__ENV object - defined in index.html:
 interface WindowWithEnv extends Window {
   __ENV?: {
-    backendURL: string, // REACT_APP_BACKEND_URL environment variable
-    sandbox: "true" | "false", // REACT_APP_SANDBOX_SDK environment variable - string, not boolean!
-  }
+    backendURL: string; // REACT_APP_BACKEND_URL environment variable
+    sandbox: "true" | "false"; // REACT_APP_SANDBOX_SDK environment variable - string, not boolean!
+  };
 }
 
 const _window: WindowWithEnv = window;
 const backendURL = _window.__ENV && _window.__ENV.backendURL;
 
-const axiosClient = axios.create({ baseURL: `${backendURL}`, timeout: 20000, withCredentials: true});
+const axiosClient = axios.create({
+  baseURL: `${backendURL}`,
+  timeout: 20000,
+  withCredentials: true,
+});
 
-const config = {headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'}};
+const config = {
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  },
+};
 
-export default function  UserToAppPayments() {
-  const { user, saveUser, showModal, saveShowModal, onModalClose } = React.useContext(UserContext) as UserContextType;
-  const [createCommunityData, setCreateCommunityData] = useState<CommunityType[] | null>(null);
+export default function UserToAppPayments() {
+  const {
+    user,
+    saveUser,
+    showModal,
+    saveShowModal,
+    onModalClose,
+  } = React.useContext(UserContext) as UserContextType;
+  const [createCommunityData, setCreateCommunityData] = useState<
+    CommunityType[] | null
+  >(null);
 
-  const orderProduct = async (memo: string, amount: number, paymentMetadata: MyPaymentMetadata) => {
-    if(user.uid === "") {
+  const orderProduct = async (
+    memo: string,
+    amount: number,
+    paymentMetadata: MyPaymentMetadata
+  ) => {
+    if (user.uid === "") {
       return saveShowModal(true);
     }
 
@@ -45,87 +74,87 @@ export default function  UserToAppPayments() {
       onReadyForServerApproval,
       onReadyForServerCompletion,
       onCancel,
-      onError
+      onError,
     };
     const payment = await window.Pi.createPayment(paymentData, callbacks);
     console.log(payment);
-  }
-  useEffect(() => {
-    console.log(user);
-  }
-  , [user]);
+  };
 
   useEffect(() => {
-    axiosClient.get('/user/me')
+    console.log(user);
+  }, [user]);
+
+  useEffect(() => {
+    axiosClient
+      .get("/user/me")
       .then((response) => {
         console.log(response);
         setCreateCommunityData(response.data);
-      })  
+      })
       .catch((error) => {
         console.log(error);
       });
-    }
-  , []);
+  }, []);
 
   return (
     <>
-      <Header/>
+      <Header />
 
       <Typography variant="h5" margin={2} color="pink">
         Profile
       </Typography>
       <Typography variant="h6" margin={2} color="black">
-      Your Communities 
-      </Typography>
-    {createCommunityData ? (
-      createCommunityData.map((community) => {
-        if (community.owner === user.uid) {
-          return (
-            <ProfileCard
-              key={community._id}
-              name={community.name}
-              _id={user.uid}
-              description={"kfdkjfdjkdfjk"}
-              owner = {community.owner}
-            />
-          );
-        }
-        return null; // Return null if the condition is not met
-      })
-    ) : (
-      <p>No community data available</p>
-    )}
-
-      <Typography variant="h6" margin={2} color="black">
-          Joined Communities
+        Your Communities
       </Typography>
       {createCommunityData ? (
-    createCommunityData.map((community) => {
-      const isUserMember = community.members.some((mem) => mem === user);
+        createCommunityData.map((community) => {
+          if (community.owner === user.uid) {
+            return (
+              <ProfileCard
+                key={community._id}
+                name={community.name}
+                _id={user.uid}
+                description={"kfdkjfdjkdfjk"}
+                owner={community.owner}
+              />
+            );
+          }
+          return null; // Return null if the condition is not met
+        })
+      ) : (
+        <p>No community data available</p>
+      )}
 
-      if (isUserMember) {
-        return (
-          <ProfileCard
-            key={community._id}
-            name={community.name}
-            _id={user.uid}
-            description={"kfdkjfdjkdfjk"}
-            owner = {community.owner}
-          />
-        );
-      }
-      
-      return null; // Return null if the condition is not met
-    })
-  ) : (
-    <p>No community data available</p>
-  )}
+      <Typography variant="h6" margin={2} color="black">
+        Joined Communities
+      </Typography>
+      {createCommunityData ? (
+        createCommunityData.map((community) => {
+          const isUserMember = community.members.some((mem) => mem === user);
 
+          if (isUserMember) {
+            return (
+              <ProfileCard
+                key={community._id}
+                name={community.name}
+                _id={user.uid}
+                description={"kfdkjfdjkdfjk"}
+                owner={community.owner}
+              />
+            );
+          }
 
-    {showModal && <SignIn onSignIn={saveUser} onModalClose={onModalClose} showModal={showModal}/>}
+          return null; // Return null if the condition is not met
+        })
+      ) : (
+        <p>No community data available</p>
+      )}
 
-    <MuiBottomNavigation />
-  </>
+      {showModal && (
+        <SignIn onSignIn={saveUser} onModalClose={onModalClose} showModal={showModal} />
+      )}
+
+      <MuiBottomNavigation />
+    </>
   );
-
 }
